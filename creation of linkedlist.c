eedf1f4535b715a44e6c640d78c6af04cbfb007b
DevikaPramod01 <115997408+DevikaPramod01@users.noreply.github.com>
2022-11-06 11:33:51 +0530

    Create creation of linkedlist.c

---
+++ creation of linkedlist.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node*link;
+}
+*start=NULL;
+void create()
+{
+    struct node*newnode,*current;
+    int i,n;
+    printf("how many nodes do you want to create\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("enter the data ");
+        scanf("%d",&newnode->data);
+        newnode->link=NULL;
+        if(start==NULL)
+        {
+            start=newnode;
+            current=newnode;
+        }
+        else
+        {
+            current->link=newnode;
+            current=newnode;
+        }
+    }
+}
+void display()
+{
+    struct node*temp;
+    temp=start;
+    while(temp!=NULL)
+    {
+        printf("linked list:%d \n",temp->data);
+        temp=temp->link;
+    }
+}
+void main()
+{
+    create();
+    display();
+}
