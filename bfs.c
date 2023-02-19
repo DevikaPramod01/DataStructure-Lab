97939d4627eb291b0bedc78842d73cf435a69676
DevikaPramod01 <115997408+DevikaPramod01@users.noreply.github.com>
2023-02-19 20:50:20 +0530

    Update bfs.c

--- bfs.c
+++ bfs.c
@@ -1,37 +1,35 @@
-#include <stdio.h>
-int a[20][20], q[20], visited[20], n, i, j, f = 0, r = -1;
-void bfs(int v)
-{
-for (i = 1; i <= n; i++)
-if (a[v][i] && !visited[i])
-q[++r] = i;
-if (f <= r)
-{
-visited[q[f]] = 1;
-bfs(q[f++]);
-}
-}
+#include<stdio.h>
 void main()
 {
-int v;
-printf("\n Enter the number of vertices:");
-scanf("%d", &n);
-for (i = 1; i <= n; i++)
-{
-q[i] = 0;
-visited[i] = 0;
-}
-printf("\n Enter graph data in matrix form:\n");
-for (i = 1; i <= n; i++)
-for (j = 1; j <= n; j++)
-scanf("%d", &a[i][j]);
-printf("\n Enter the starting vertex:");
-scanf("%d", &v);
-bfs(v);
-printf("\n BFS result:\n\n");
-for (i = 1; i <= n; i++)
-if (visited[i])
-printf("%d\t", i);
-else
-printf("\n BFS is not possible");
+    int v,n,g[10][10],visited[10]={0},i,j;
+    int queue[10],r=-1,f=-1;
+    printf("enter the number of vertices:");
+    scanf("%d",&n);
+    printf("enter the adjacency matrix:");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&g[i][j]);
+        }
+    }
+    printf("enter the starting vertex:");
+    scanf("%d",&v);
+    printf("result ");
+    queue[++r]=v;
+    ++f;
+    visited[v]=1;
+    while(r>=f)
+    {
+        v=queue[f++];
+        printf(" %d \t ",v);
+        for(i=0;i<n;i++)
+        {
+            if(g[v][i]==1 && visited[i]==0)
+            {
+                queue[++r]=i;
+                visited[i]=1;
+            }
+        }
+    }
 }
